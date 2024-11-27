2f40ee46d195deae0fae89cce7a01776e7ebb7d4
Unagi-zoso <Unagi.zoso@gmail.com>
2024-11-27 10:56:02 +0900

    Fix 누락된 new 키워드 추가

--- controller/userController.js
+++ controller/userController.js
@@ -39,7 +39,7 @@ class UserController {
             user.nickname !== nickname &&
             this.userDao.findByNickname(nickname)
         ) {
-            throw ErrorWrapper(200, 4009, '이미 사용 중인 닉네임입니다', null);
+            throw new ErrorWrapper(200, 4009, '이미 사용 중인 닉네임입니다', null);
         }
 
         let profileImageUrl = user.profileImageUrl;
